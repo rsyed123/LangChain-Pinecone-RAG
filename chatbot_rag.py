f6c84120021e2d54c5b7f629f17687367e2e0f69
rsyed123 <rsyed123@gmail.com>
2026-03-02 12:47:53 -0500

    commits made by riaz

--- chatbot_rag.py
+++ chatbot_rag.py
@@ -27,6 +27,9 @@ index = pc.Index(index_name)
 embeddings = OpenAIEmbeddings(model="text-embedding-3-large",api_key=os.environ.get("OPENAI_API_KEY"))
 vector_store = PineconeVectorStore(index=index, embedding=embeddings)
 
+print("Key loaded:", os.environ.get("OPENAI_API_KEY")[:10])
+
+
 # initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
